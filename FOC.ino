e11ea3239f105ab1fc88477fc196cc84439b0ea2
geofrancis <o.o.o.o@hotmail.co.uk>
2026-06-22 10:01:03 +0100

    Refactor FOC setup and telemetry functions

--- FOC.ino
+++ FOC.ino
@@ -9,18 +9,7 @@ void FOC_SETUPL() {
   motor.linkSensor(&sensor);
   driver.voltage_power_supply = 12;
   motor.voltage_sensor_align = 1;
-
-  //motor.foc_modulation = FOCModulationType::SinePWM;
-  //motor.foc_modulation = FOCModulationType::SpaceVectorPWM;  // Recommended for efficiency
-  // motor.foc_modulation = FOCModulationType::Trapezoid_120;
-  // motor.foc_modulation = FOCModulationType::Trapezoid_150;
   motor.controller = MotionControlType::torque;
-  // set FOC loop to be used
-  // MotionControlType::torque
-  // MotionControlType::velocity
-  // MotionControlType::angle
-  // MotionControlType::angle_nocascade
-
   motor.useMonitoring(Serial);
   motor.init();
   //commect out for auto detect
@@ -40,11 +29,9 @@ void FOC_SETUPR() {
   motor1.linkSensor(&sensor1);
   driver1.voltage_power_supply = 12;
   motor1.voltage_sensor_align = 1;
-  // motor1.foc_modulation = FOCModulationType::SpaceVectorPWM;
   motor1.controller = MotionControlType::torque;
   motor1.useMonitoring(Serial);
   motor1.init();
-
   //commect out for auto detect
   motor1.sensor_direction = Direction::CCW;  // CW or CCW
   motor1.zero_electric_angle = 4.19;
@@ -52,46 +39,6 @@ void FOC_SETUPR() {
 
   motor1.initFOC();
 }
-
-
-
-
-/*
-
-
-
-
-void FOC_SETUPR() {
-  sensor1.init();
-  sensor1.enableInterrupts(doA1, doB1, doC1);
-  motor1.linkSensor(&sensor1);
-  driver1.voltage_power_supply = 36;
-  motor1.voltage_sensor_align = 1;
-
-
-  motor.controller = MotionControlType::velocity;
-
-  // velocity PI controller parameters
-  // default P=0.5 I = 10
-  motor.PID_velocity.P = 0.2;
-  motor.PID_velocity.I = 20;
-  //default voltage_power_supply
-  motor.voltage_limit = 6;
-  motor.LPF_velocity.Tf = 0.01;
-
-
-  motor1.useMonitoring(Serial);
-  motor1.init();
-  motor1.sensor_direction = Direction::CCW;  // CW or CCW
-  motor1.initFOC();
-}
-
-
-
-
-
-
-*/
 
 
 
